5b722faa865d19e5c9b7188a1a0b4f3b1b043b5c
Pierrick GRUWIER <pierrick.gruwier@decathlon.com>
2024-11-29 14:17:03 +0100

    [1] - Inital commit with dependencies
    Signed-off-by: Pierrick GRUWIER <pierrick.gruwier@decathlon.com>

---
+++ app/src/main/java/com/gruwier/ktordemo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.gruwier.ktordemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gruwier.ktordemo.ui.theme.KtorDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KtorDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Modifier
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
